d00e38d7413f8209f29e521ee25f7f1e6525650f
Jared Carlson <jared.carlson23@gmail.com>
2024-11-23 15:37:37 -0500

    Added a contains_els utility

--- lib/lists.ml
+++ lib/lists.ml
@@ -1,12 +1,32 @@
+(** Determine if a given list [lst] has a prefix [pre] -> bool **)
 let rec has_prefix lst pre = 
   match lst, pre with 
   | _, [] -> true
   | [], _ -> false 
   | h1::tl1, h2::tl2 -> h1 = h2 && has_prefix tl1 tl2
 
+  (** Determine if a given list [lst] contains a sublist, or slice, 
+    [slc] in it as opposed to containing the elements of the sublist
+    or slice. 
+     -- Note: the elements in the slice [slc] must be in ascending
+    order
+  **)
 let rec contains_slice lst slc = 
   match lst, slc with 
   | [], [] -> true 
   | [], _ -> false 
   | _::tl, _  -> has_prefix lst slc || contains_slice tl slc 
+
+  (** Determine if a given list [lst] has the elements of a sublist, or 
+    slice [slc] in it; regardless of whether they occur sequentially
+    or not
+    -- Note: the elements in the slice [slc] must be in ascending
+    order
+  **)
+let rec contains_els lst slc = 
+  match lst, slc with
+  | _, [] -> true
+  | [], _ -> false 
+  | h1::t1, h2::t2 when h1 = h2 -> contains_els t1 t2 
+  | _::t1, _ -> contains_els t1 slc 
 
--- test/test_utilities.ml
+++ test/test_utilities.ml
@@ -27,11 +27,28 @@ let test_contains_char_slice () =
   (* end of test *)
   ()
 
+  let test_contains_els () =
+    let lst1 = [1; 2; 3; 4; 5; 6; 7; 8; 9] in
+    let needle1 = [1; 2; 3] in 
+    let needle2 = [7; 8; 9] in
+    let needle3 = [4;7] in 
+    let bad_needle1 = [3; 11] in
+    let bad_needle2 = [0; 2] in
+  
+    check bool "Lst contains [1; 2; 3]" true (contains_els lst1 needle1);
+    check bool "Lst did not contain [3; 2]" false (contains_els lst1 bad_needle1);
+    check bool "Lst contains [7; 8; 9]" true (contains_els lst1 needle2);
+    check bool "Lst did not contain [2; 4]" false (contains_els lst1 bad_needle2);
+    check bool "Lst contains [4;5]" true (contains_els lst1 needle3);
+    (* End of test *)
+    ()
+
 let () = 
   let open Alcotest in 
   run "Tests" [
     "List Tests", [
       test_case "Integer Slice tests" `Quick test_contains_slice; 
       test_case "Char Slice tests" `Quick test_contains_char_slice; 
+      test_case "Test Slice element tests" `Quick test_contains_els;
     ];
   ]
