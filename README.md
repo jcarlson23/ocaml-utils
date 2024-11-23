7a3621530cd3acd6f2e1b37204ac53a79abef823
Jared Carlson <jared.carlson23@gmail.com>
2024-11-23 10:32:56 -0500

    Just a quick addition

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # OCaml Utilities
+
+These are a collection of utilities (in Ocaml) to parse various
+structures relating to an AST (or similar).
+
--- test/test_utilities.ml
+++ test/test_utilities.ml
@@ -5,6 +5,7 @@ let test_contains_slice () =
   let lst1 = [1; 2; 3; 4; 5; 6; 7; 8; 9] in
   let needle1 = [1; 2; 3] in 
   let needle2 = [7; 8; 9] in
+  let needle3 = [4;5] in 
   let bad_needle1 = [3; 2] in
   let bad_needle2 = [2; 4] in
 
@@ -12,6 +13,7 @@ let test_contains_slice () =
   check bool "Lst did not contain [3; 2]" false (contains_slice lst1 bad_needle1);
   check bool "Lst contains [7; 8; 9]" true (contains_slice lst1 needle2);
   check bool "Lst did not contain [2; 4]" false (contains_slice lst1 bad_needle2);
+  check bool "Lst contains [4;5]" true (contains_slice lst1 needle3);
   (* End of test *)
   ()
 
